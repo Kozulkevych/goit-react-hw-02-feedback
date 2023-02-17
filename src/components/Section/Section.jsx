caa0278f941c34c619166f3f90b0c12ab0029d2e
Kozulkevych Nadiia <nkozulkevych@gmail.com>
2023-02-17 05:20:25 +0200

    add solution

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import { Container, Title, Text } from './Section.styled';
+
+const Section = ({ title, children }) => (
+  <Container>
+    <Title>
+      <Text>{title}</Text>
+    </Title>
+    {children}
+  </Container>
+);
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+export default Section;
